b94f96e1585c9eb86deea642a420b452fa1a3a5f
PiotrW0jcik <p.w9714x3@gmail.com>
2020-09-23 14:00:00 +0100

    Added basic register and login component && service

--- Dimico.Client/src/app/login/login.component.ts
+++ Dimico.Client/src/app/login/login.component.ts
@@ -1,15 +1,35 @@
+import { AuthService } from './../services/auth.service';
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder, private AuthService: AuthService) {
+    this.loginForm = this.fb.group({
+      'username': [ '', Validators.required ],
+      'password': [ '', Validators.required ]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    this.AuthService.login(this.loginForm.value).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  get username(){
+    return this.loginForm.get('username');
+  }
+
+  get password(){
+    return this.loginForm.get('password');
   }
 
 }
