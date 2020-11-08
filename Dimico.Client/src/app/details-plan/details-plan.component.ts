ead168647c9e81407cd8f02a0550b23c21401647
PiotrW0jcik <p.w9714x3@gmail.com>
2020-11-08 10:20:00 +0100

    Refactored details plan nested subscriptions

--- Dimico.Client/src/app/details-plan/details-plan.component.ts
+++ Dimico.Client/src/app/details-plan/details-plan.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Plan } from '../models/Plan';
 import { PlanService } from '../services/plan.service';
+import { map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-details-plan',
@@ -12,11 +13,20 @@ export class DetailsPlanComponent implements OnInit {
   id: string;
   plan: Plan;
   constructor(private route: ActivatedRoute, private planService: PlanService) { 
-    this.route.params.subscribe(res =>{
-      this.id = res['id'];
-      this.planService.getPlan(this.id).subscribe(res =>{
-        this.plan = res;
-      })
+    // this.route.params.subscribe(res =>{
+    //   this.id = res['id'];
+    //   this.planService.getPlan(this.id).subscribe(res =>{
+    //     this.plan = res;
+    //   })
+    //})
+    this.fetchData()
+  }
+  fetchData(){
+    this.route.params.pipe(map( params => {
+      const id = params ['id'];
+      return id;
+    }), mergeMap(id => this.planService.getPlan(id))).subscribe(res => {
+      this.plan = res;
     })
   }
 
